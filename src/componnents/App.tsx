60b1621c84ccdecef2cebcd81c1a33aff022cbc0
hod25 <hod7112000@gmail.com>
2025-02-14 09:34:17 +0200

    add routes

--- src/componnents/App.tsx
+++ src/componnents/App.tsx
@@ -1,11 +1,22 @@
-import PostsList from "./PostsList"
-
-
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "../pages/Home";
+import Login from "../pages/login";
+import Recipe from "./Recipe";
+import Register from "./RegistrationForm";
+// import Profile from "./pages/Profile";
 
 function App() {
-  return (
-    <PostsList />
-  )
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/recipe" element={<Recipe />} />
+                <Route path="/register" element={<Register />} />
+                {/* <Route path="/profile" element={<Profile />} /> */}
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+export default App;
