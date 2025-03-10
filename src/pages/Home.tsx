e1418475ab6cb5e237ac7b14d04603b4eccedd5f
hod25 <hod7112000@gmail.com>
2025-03-10 10:42:06 +0200

    token

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -57,6 +57,12 @@ const Home: FC = () => {
       console.error("Title and either post content or image are required.");
       return;
     }
+
+    const token = localStorage.getItem("token");
+    if (!token) {
+      console.error("No token found in localStorage.");
+      return;
+    }
   
     try {
       const postData = {
@@ -67,7 +73,7 @@ const Home: FC = () => {
           .filter(ingredient => ingredient)),
         tags: JSON.stringify(selectedAllergies) || [],
         likes: 0,
-        owner: "67bf367a6596d896e4ffed31"
+        owner: token,
       };
   
       const response = await axios.post(
